a4a077e37d10463d75148a71ba90cb90ee0a1037
drewdoesdev <ataylor2247@gmail.com>
2024-02-28 09:57:11 -0600

    Initial app setup: added tailwind, created builerplate folders for components.

--- App.tsx
+++ App.tsx
@@ -4,8 +4,10 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+      {/** Search */}
+      {/** Class Filters */}
+      {/** Spell Cards */}
     </View>
   );
 }
